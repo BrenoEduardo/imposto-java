0902544884aeeb4ec1119ae5e1e1c53606104f4f
BrenoEduardo <brenoeducm@hotmail.com>
2020-05-12 14:49:47 -0300

    Leitura dos dados

--- src/imposto/impostoproject.java
+++ src/imposto/impostoproject.java
@@ -1,9 +1,31 @@
 package imposto;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class impostoproject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double ras, raps, ragc, gn, ge;
+		
+		System.out.printf("Renda anual com salário: ");
+		ras = sc.nextDouble();
+		System.out.printf("Renda anual com prestação de serviço: ");
+		raps = sc.nextDouble();
+		System.out.printf("Renda anual com ganho de capital: ");
+		ragc = sc.nextDouble();
+		System.out.printf("Gastos médicos: ");
+		gn = sc.nextDouble();
+		System.out.printf("Gastos educacionais: ");
+		ge = sc.nextDouble();
+		
+		
+		
+		sc.close();
 
 	}
 
