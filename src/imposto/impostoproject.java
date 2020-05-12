aa415a764e28701d1a621be21a707aa8659503be
BrenoEduardo <brenoeducm@hotmail.com>
2020-05-12 15:22:38 -0300

    Consolidado de renda

--- src/imposto/impostoproject.java
+++ src/imposto/impostoproject.java
@@ -10,7 +10,9 @@ public class impostoproject {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		double ras, raps, ragc, gn, ge;
+		double ras, raps, ragc, gn, ge, isalario, iservicos, icapital;
+		
+		//LEITURA DE DADOS
 		
 		System.out.printf("Renda anual com salário: ");
 		ras = sc.nextDouble();
@@ -22,8 +24,39 @@ public class impostoproject {
 		gn = sc.nextDouble();
 		System.out.printf("Gastos educacionais: ");
 		ge = sc.nextDouble();
+		// CONSOLIDAÇÃO DE RENDA
 		
+		System.out.println();
+		System.out.println("RELATÓRIO DE IMPOSTO DE RENDA");
 		
+		System.out.println();
+		System.out.println("CONSOLIDADO DE RENDA:");
+		
+		if( ras < 36000.00) {
+			System.out.println("Imposto sobre o salário: 0.00");
+		}
+		else if( ras > 36000.00 && ras < 60000.00) {
+			isalario = ras * 10 /100;
+			System.out.println("Imposto sobre o salário: " + isalario);
+		}
+		else {
+			isalario = ras * 20 /100;
+			System.out.println("Imposto sobre o salário: " + isalario);
+		}
+		if (raps > 0.00 ) {
+			iservicos = raps * 15 /100;
+			System.out.println("Imposto sobre serviços: " + iservicos);
+		}
+		else {
+			System.out.println("Imposto sobre serviços: 0.00");
+		}
+		if ( ragc > 0) {
+			icapital = ragc * 20 /100;
+			System.out.println("Imposto sobre ganho de capital:" + icapital);
+		}
+		else {
+			System.out.println("Imposto sobre ganho de capital: 0.00");
+		}
 		
 		sc.close();
 
